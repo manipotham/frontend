da661b99a3a1d1f2e02a955fbdad643c1adc7913
manipotham <manikantapothamsetti897@gmail.com>
2025-04-22 14:04:42 +0530

    updated front end url

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -6,5 +6,5 @@ import { Injectable } from '@angular/core';
 export class Environment {
   // Api:string = "https://y9u1c7vqx4.execute-api.ap-south-1.amazonaws.com/Prod"
   Api:string = "https://d3q4n7cd6yvt05.cloudfront.net/Prod"
-  Main:string = "https://d3cf6hrf5sigu6.cloudfront.net"
+  Main:string = "ttps://frontend-eight-blond-88.vercel.app"
 }
